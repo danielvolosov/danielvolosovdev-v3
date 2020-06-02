9e3090ccb7a5a4b130046aac112639accada3e74
daniel <danielvolosov@outlook.com>
2020-06-02 18:32:16 -0400

    update constants to reflect social media and update platforms

--- src/components/Platforms.js
+++ src/components/Platforms.js
@@ -6,38 +6,48 @@ import {
     Card
   } from 'rebass';
 
+import * as siteConstants from '../shared/Constants';
+
+
 function Platforms() {
-    return (
+  const twitter = siteConstants.TWITTER;
+  const linkedin = siteConstants.LINKEDIN;
+  const github = siteConstants.GITHUB;
+  const instagram = siteConstants.INSTAGRAM;
+  const facebook = siteConstants.FACEBOOK;
+  const reddit = siteConstants.REDDIT;
+  
+  return (
 <Flex py={3}>
   <Card sx={{
     display: 'inline-block',
     bg: 'black'}} width={[ 256, 256 ]} mx='auto'>
-    <Image src={require('../shared/icons/twitter.svg')} />
+    <a href={ twitter } target="_blank"><Image src={require('../shared/icons/twitter.svg')} /></a>
   </Card>
   <Card  sx={{
     display: 'inline-block',
     bg: 'black'}} width={[ 256, 256 ]} mx='auto'>
-  <Image src={require('../shared/icons/linkedin.svg')} />
+  <a href={ linkedin } target="_blank"><Image src={require('../shared/icons/linkedin.svg')} /></a>
   </Card>
   <Card  sx={{
     display: 'inline-block',
     bg: 'black'}} width={[ 256, 256 ]} mx='auto'>
-  <Image src={require('../shared/icons/github.svg')} />
+  <a href={ github } target="_blank"><Image src={require('../shared/icons/github.svg')} /></a>
   </Card>
   <Card  sx={{
     display: 'inline-block',
     bg: 'black'}} width={[ 256, 256 ]} mx='auto'>
-  <Image src={require('../shared/icons/instagram.svg')} />
+  <a href={ instagram } target="_blank"><Image src={require('../shared/icons/instagram.svg')} /></a>
   </Card>
   <Card  sx={{
     display: 'inline-block',
     bg: 'black'}} width={[ 256, 256 ]} mx='auto'>
-  <Image src={require('../shared/icons/facebook.svg')} />
+  <a href={ facebook } target="_blank"><Image src={require('../shared/icons/facebook.svg')} /></a>
   </Card>
   <Card  sx={{
     display: 'inline-block',
     bg: 'black'}} width={[ 256, 256 ]} mx='auto'>
-  <Image src={require('../shared/icons/reddit.svg')} />
+  <a href={ reddit } target="_blank"><Image src={require('../shared/icons/reddit.svg')} /></a>
   </Card>
 </Flex>
     );
