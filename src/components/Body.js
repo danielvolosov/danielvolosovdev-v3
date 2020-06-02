d5519451ce0a2c6c8ef894cb9a7c0d685f76b895
daniel <danielvolosov@outlook.com>
2020-06-01 21:01:14 -0400

    constants for organization

--- src/components/Body.js
+++ src/components/Body.js
@@ -5,13 +5,16 @@ import {
     Text
   } from 'rebass';
 
+import * as siteConstants from '../shared/Constants';
+
 function Body() {
+    const bodyContent = siteConstants.BODY;
     return (
 <div classname="flexPadding">
 <Flex mx={-2}>
   <Box width={2/2} px={2} py={3}>
     <Text p={1} color='background' bg='black'>
-    Software Engineer with 3+ years as a consultant. In the private sector, Daniel has developed software for several different industries including health insurance, wholesale distribution, tech startups and restaurant chains. He is experienced with building global applications for organizations ranging from the largest global insurance provider, to a small cutting-edge startup. Daniel is currently working in the federal sector.
+        { bodyContent }
     </Text>
   </Box>
 </Flex>
