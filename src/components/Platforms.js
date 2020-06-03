1543fcd0794ccb7599a3c821c5eb13c68149adf9
daniel <danielvolosov@outlook.com>
2020-06-03 16:16:37 -0400

    dynamically display platforms

--- src/components/Platforms.js
+++ src/components/Platforms.js
@@ -4,13 +4,56 @@ import {Flex, Image, Card} from "rebass";
 
 import * as siteConstants from "../shared/Constants";
 
-function Platforms() {
   const twitter = siteConstants.TWITTER;
   const linkedin = siteConstants.LINKEDIN;
   const github = siteConstants.GITHUB;
   const instagram = siteConstants.INSTAGRAM;
   const facebook = siteConstants.FACEBOOK;
   const reddit = siteConstants.REDDIT;
+
+  const socialMediaIcons = [
+  require("../shared/icons/twitter.svg"),
+  require("../shared/icons/linkedin.svg"),
+  require("../shared/icons/github.svg"),
+  require("../shared/icons/instagram.svg"),
+  require("../shared/icons/facebook.svg"),
+  require("../shared/icons/reddit.svg")
+  ];
+
+  export const socialMedia = [
+    {
+      id: 1,
+      name: "Twitter",
+      src: socialMediaIcons[0]
+    },
+    {
+      id: 2,
+      name: "Linkedin",
+      src: socialMediaIcons[1]
+    },
+    {
+      id: 3,
+      name: "Github",
+      src: socialMediaIcons[2]
+    },
+    {
+      id: 4,
+      name: "Instagram",
+      src: socialMediaIcons[3]
+    },
+    {
+      id: 5,
+      name: "Facebook",
+      src: socialMediaIcons[4]
+    },
+    {
+      id: 6,
+      name: "Reddit",
+      src: socialMediaIcons[5]
+    }
+  ];
+
+function Platforms() {
 
   return (
     <Flex py={3}>
@@ -22,70 +65,11 @@ function Platforms() {
         width={[256, 256]}
         mx="auto"
       >
-        <a href={twitter} target="_blank">
-          <Image src={require("../shared/icons/twitter.svg")} />
-        </a>
+          {socialMedia.map(p => {
+            return <img key={p.id} src={p.src} alt="Picture unavailable" />;
+          })}
       </Card>
-      <Card
-        sx={{
-          display: "inline-block",
-          bg: "black",
-        }}
-        width={[256, 256]}
-        mx="auto"
-      >
-        <a href={linkedin} target="_blank">
-          <Image src={require("../shared/icons/linkedin.svg")} />
-        </a>
-      </Card>
-      <Card
-        sx={{
-          display: "inline-block",
-          bg: "black",
-        }}
-        width={[256, 256]}
-        mx="auto"
-      >
-        <a href={github} target="_blank">
-          <Image src={require("../shared/icons/github.svg")} />
-        </a>
-      </Card>
-      <Card
-        sx={{
-          display: "inline-block",
-          bg: "black",
-        }}
-        width={[256, 256]}
-        mx="auto"
-      >
-        <a href={instagram} target="_blank">
-          <Image src={require("../shared/icons/instagram.svg")} />
-        </a>
-      </Card>
-      <Card
-        sx={{
-          display: "inline-block",
-          bg: "black",
-        }}
-        width={[256, 256]}
-        mx="auto"
-      >
-        <a href={facebook} target="_blank">
-          <Image src={require("../shared/icons/facebook.svg")} />
-        </a>
-      </Card>
-      <Card
-        sx={{
-          display: "inline-block",
-          bg: "black",
-        }}
-        width={[256, 256]}
-        mx="auto"
-      >
-        <a href={reddit} target="_blank">
-          <Image src={require("../shared/icons/reddit.svg")} />
-        </a>
-      </Card>
+      
     </Flex>
   );
 }
