606fbb58229f2fcf33ef71f542fa78f741a4de55
daniel <danielvolosov@outlook.com>
2020-06-03 23:34:19 -0400

    cleanup import

--- src/components/Platforms.js
+++ src/components/Platforms.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Platforms.css";
-import {Flex, Image, Card} from "rebass";
+import { Image } from "rebass";
 
 import * as siteConstants from "../shared/Constants";
 
