06d29d3154c9b7d2c77e2929b197064753bf52fb
daniel <danielvolosov@outlook.com>
2020-10-03 19:40:17 -0400

    uses link added

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,6 +1,10 @@
 import React from "react";
 import "./Header.css";
-import {Box, Flex} from "rebass";
+import {Box, Flex, Link as RebassLink} from "rebass";
+import {
+  Link as ReactLink,
+  BrowserRouter as Router
+} from "react-router-dom";
 
 import * as siteConstants from "../../shared/Constants";
 
@@ -8,12 +12,18 @@ function Header() {
   const nameContent = siteConstants.NAME;
 
   return (
-    <Flex className="header" px={1} color="white" bg="black" alignItems="center">
+    <Flex className="header headerContainer" px={1} color="white" bg="black" alignItems="center">
       <h1 variant="nav" href="#">
         > {nameContent}
         <span className="blinking">_</span>
       </h1>
       <Box mx="auto" />
+      <Router>
+      <div className="links">
+      <div className="linkItem"><RebassLink variant='nav'><ReactLink to="/">/home</ReactLink></RebassLink></div>
+      <div className="linkItem"><RebassLink variant='nav'><ReactLink to="/uses">/uses</ReactLink></RebassLink></div>
+      </div>
+      </Router>
     </Flex>
   );
 }
