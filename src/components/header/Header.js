33de0fb43eae129f24659cecab8e19d7c8901a59
daniel <danielvolosov@outlook.com>
2020-10-03 19:40:29 -0400

    styled

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,10 +1,7 @@
 import React from "react";
 import "./Header.css";
 import {Box, Flex, Link as RebassLink} from "rebass";
-import {
-  Link as ReactLink,
-  BrowserRouter as Router
-} from "react-router-dom";
+import {Link as ReactLink, BrowserRouter as Router} from "react-router-dom";
 
 import * as siteConstants from "../../shared/Constants";
 
@@ -12,17 +9,31 @@ function Header() {
   const nameContent = siteConstants.NAME;
 
   return (
-    <Flex className="header headerContainer" px={1} color="white" bg="black" alignItems="center">
+    <Flex
+      className="header headerContainer"
+      px={1}
+      color="white"
+      bg="black"
+      alignItems="center"
+    >
       <h1 variant="nav" href="#">
         > {nameContent}
         <span className="blinking">_</span>
       </h1>
       <Box mx="auto" />
       <Router>
-      <div className="links">
-      <div className="linkItem"><RebassLink variant='nav'><ReactLink to="/">/home</ReactLink></RebassLink></div>
-      <div className="linkItem"><RebassLink variant='nav'><ReactLink to="/uses">/uses</ReactLink></RebassLink></div>
-      </div>
+        <div className="links">
+          <div className="linkItem">
+            <RebassLink variant="nav">
+              <ReactLink to="/">/home</ReactLink>
+            </RebassLink>
+          </div>
+          <div className="linkItem">
+            <RebassLink variant="nav">
+              <ReactLink to="/uses">/uses</ReactLink>
+            </RebassLink>
+          </div>
+        </div>
       </Router>
     </Flex>
   );
