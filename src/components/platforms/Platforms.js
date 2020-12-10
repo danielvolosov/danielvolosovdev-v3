7677228645b2dbc5dc4f950410ce664e020d76bf
daniel <danielvolosov@outlook.com>
2020-12-10 15:02:02 -0500

    update dependencies and constant

--- src/components/platforms/Platforms.js
+++ src/components/platforms/Platforms.js
@@ -1,59 +1,61 @@
-import React from "react";
-import "./Platforms.css";
-import {Image} from "rebass";
+import React from 'react';
+import './Platforms.css';
+import { Image } from 'rebass';
 
-import * as siteConstants from "../../shared/Constants";
+import * as siteConstants from '../../shared/Constants';
 
 const socialMediaIcons = [
-  require("../../shared/icons/twitter.svg"),
-  require("../../shared/icons/linkedin.svg"),
-  require("../../shared/icons/github.svg"),
-  require("../../shared/icons/medium.svg"),
-  require("../../shared/icons/instagram.svg"),
+	require('../../shared/icons/twitter.svg'),
+	require('../../shared/icons/linkedin.svg'),
+	require('../../shared/icons/github.svg'),
+	require('../../shared/icons/medium.svg'),
+	require('../../shared/icons/instagram.svg'),
 ];
 
 const socialMedia = [
-  {
-    id: 1,
-    mediaLink: siteConstants.TWITTER,
-    src: socialMediaIcons[0],
-  },
-  {
-    id: 2,
-    mediaLink: siteConstants.LINKEDIN,
-    src: socialMediaIcons[1],
-  },
-  {
-    id: 3,
-    mediaLink: siteConstants.GITHUB,
-    src: socialMediaIcons[2],
-  },
-  {
-    id: 4,
-    mediaLink: siteConstants.MEDIUM,
-    src: socialMediaIcons[3],
-  },
-  {
-    id: 5,
-    mediaLink: siteConstants.INSTAGRAM,
-    src: socialMediaIcons[4],
-  },
+	{
+		id: 1,
+		mediaLink: siteConstants.TWITTER,
+		src: socialMediaIcons[0],
+	},
+	{
+		id: 2,
+		mediaLink: siteConstants.LINKEDIN,
+		src: socialMediaIcons[1],
+	},
+	{
+		id: 3,
+		mediaLink: siteConstants.GITHUB,
+		src: socialMediaIcons[2],
+	},
+	{
+		id: 4,
+		mediaLink: siteConstants.MEDIUM,
+		src: socialMediaIcons[3],
+	},
+	{
+		id: 5,
+		mediaLink: siteConstants.INSTAGRAM,
+		src: socialMediaIcons[4],
+	},
 ];
 
-function Platforms() {
-  return (
-    <div className="platformContainer">
-      {socialMedia.map((p) => {
-        return (
-          <div className="platforms">
-            <a href={p.mediaLink}>
-              <Image key={p.id} src={p.src} alt="Picture unavailable" />
-            </a>
-          </div>
-        );
-      })}
-    </div>
-  );
+export default function Platforms() {
+	return (
+		<div className='platformContainer'>
+			{socialMedia.map((icon) => {
+				return (
+					<div className='platforms'>
+						<a href={icon.mediaLink}>
+							<Image
+								key={icon.id}
+								src={icon.src}
+								alt='Social Media Icon Photo Unavailable'
+							/>
+						</a>
+					</div>
+				);
+			})}
+		</div>
+	);
 }
-
-export default Platforms;
--- src/shared/Constants.js
+++ src/shared/Constants.js
@@ -1,15 +1,15 @@
 // Components
-export const NAME = "danielvolosov";
+export const NAME = 'danielvolosov';
 export const BODY =
-  "Software Engineer with 3+ years of experience as a consultant. In the private sector, Daniel has developed software for several different industries including health insurance, wholesale distribution, tech startups and restaurant chains. He is experienced with building applications for organizations ranging from the largest global insurance provider, to a small cutting-edge startup. Daniel is currently working in the federal sector.";
+	'Software Engineer with 3+ years of experience as a consultant. In the private sector, Daniel has developed software for several different industries including health insurance, wholesale distribution, tech startups and restaurant chains. He is experienced with building applications for organizations ranging from the largest global insurance provider, to a small cutting-edge startup. Daniel is currently working in the federal sector.';
 export const BODY_HOBBIES =
-  "In his spare time, Daniel enjoys learning world history & new spoken languages, planning his future travels, working on projects involving new technologies, playing acoustic guitar, listening to a variety of music (Folk, Rock, Metal & Electronic), reading both fantasy and non-fiction, watching scary movies and spending some time in the gym. He is currently open to contributing to new open-source projects in his free time as well.";
+	'In his spare time, Daniel enjoys learning world history & new spoken languages, planning his future travels, working on projects involving new technologies, playing acoustic guitar, listening to a variety of music (Folk, Rock, Metal & Electronic), reading both fantasy and non-fiction, watching scary movies and spending some time in the gym. He is currently open to contributing to new open-source projects in his free time as well.';
 export const CONTACT =
-  "To get in contact - send a Tweet/DM @danielvolosov on Twitter or LinkedIn.";
+	'For personal inquiries - send a Tweet/DM @danielvolosov on Twitter. For professional inquiries - please send a message and connection request on LinkedIn.';
 
 // Social Media
-export const TWITTER = "http://www.twitter.com/danielvolosov";
-export const INSTAGRAM = "http://www.instagram.com/danielvolosov";
-export const LINKEDIN = "http://www.linkedin.com/in/danielvolosov";
-export const GITHUB = "http://www.github.com/danielvolosov";
-export const MEDIUM = "https://www.medium.com/@danielvolosov";
+export const TWITTER = 'http://www.twitter.com/danielvolosov';
+export const INSTAGRAM = 'http://www.instagram.com/danielvolosov';
+export const LINKEDIN = 'http://www.linkedin.com/in/danielvolosov';
+export const GITHUB = 'http://www.github.com/danielvolosov';
+export const MEDIUM = 'https://www.medium.com/@danielvolosov';
