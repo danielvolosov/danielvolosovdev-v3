b40d7497af9b6e53cd0b9242608b074620eaede9
daniel <danielvolosov@outlook.com>
2021-04-12 20:36:56 -0400

    experience update

--- src/shared/Constants.js
+++ src/shared/Constants.js
@@ -1,9 +1,9 @@
 // Components
 export const NAME = 'danielvolosov';
 export const BODY =
-	'Daniel is a Software Engineer with nearly 4-years of experience as a consultant. Daniel has developed software for clients including the largest insurance company, wholesale roofing provider, innovative startup, popular restaurant chain and federal department. Daniel is currently seeking a new opportunity to contribute to the growth of an organization for an extended period of time. He also creates video & written technical content for his blog, Subtle Algorithm. Please view the links below for more information.';
+	'Daniel is a Software Engineer with nearly 4-years of experience as a consultant. Daniel has developed software for clients including a large insurance organization, wholesale roofing provider, innovative startup, national restaurant chain and federal department. He also creates video & written technical content for his blog, Subtle Algorithm. Please view the links below for more information.';
 export const BODY_HOBBIES =
-	'In his spare time, Daniel enjoys learning world history & new spoken languages, playing video games, working on projects involving new technologies, playing acoustic guitar, listening to a variety of music (Folk, Rock, Metal & Electronic), reading both fantasy and non-fiction, watching scary movies and spending some time in the gym. He is currently open to contributing to new open-source projects in his free time as well.';
+	'In his spare time, Daniel enjoys learning world history & new spoken languages, playing computer games, working on new technical projects, playing acoustic guitar, listening to a variety of music (Folk, Rock, Metal & Electronic), reading both fantasy and non-fiction, watching scary movies, spending time outdoors and more recently - becoming a better runner & cyclist.';
 export const CONTACT =
 	'For personal or professional inquiries - please send a LinkedIn message or tweet @danielvolosov on Twitter.';
 
